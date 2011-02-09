f38ef208319f12c29a90ca29eb0a6311b2afb9d3
Marcin Michalowski and Marcin Nowicki <daan+pr0d1r2+experteer-devs@ragnarson.com>
2011-02-09 09:54:19 +0100

    Clean code from redundant spaces.

--- lib/countries/country.rb
+++ lib/countries/country.rb
@@ -2,7 +2,7 @@ module ISO3166; end
 
 class ISO3166::Country
   attr_reader :data
-   
+
   AttrReaders = [
     :number,
     :alpha2,
@@ -20,37 +20,37 @@ class ISO3166::Country
     :national_prefix,
     :address_format
   ]
-  
+
   AttrReaders.each do |meth|
     define_method meth do
       @data[meth.to_s]
     end
   end
-  
+
   Data = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'data', 'countries.yaml')) || {}
   Names = Data.map {|k,v| [v['name'],k]}.sort
   NameIndex = Hash[*Names.flatten]
-    
+
   def initialize(country_code)
     @data = Data[country_code]
   end
-  
+
   def currency
     ISO4217::Currency.from_code(@data['currency'])
   end
-  
+
   def self.search(query)
     self.new(query)
   end
-  
+
   def self.[](query)
     self.search(query)
   end
-  
+
   def subdivisions
     @subdivisions ||= subdivisions? ? YAML.load_file(File.join(File.dirname(__FILE__), '..', 'data', 'subdivisions', "#{alpha2}.yaml")) : {}
   end
-  
+
   def subdivisions?
     File.exist?(File.join(File.dirname(__FILE__), '..', 'data', 'subdivisions', "#{alpha2}.yaml"))
   end
--- spec/country_spec.rb
+++ spec/country_spec.rb
@@ -3,31 +3,31 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe ISO3166::Country do
 
   let(:country) { ISO3166::Country.search('US') }
-  
+
   it 'should return 3166 number' do
     country.number.should == '840'
   end
-  
+
   it 'should return 3166 alpha2 code' do
     country.alpha2.should == 'US'
   end
-  
+
   it 'should return 3166 alpha3 code' do
     country.alpha3.should == 'USA'
   end
-  
+
   it 'should return 3166 name' do
     country.name.should == 'United States'
   end
-  
+
   it 'should return alternate names' do
     country.names.should == ["United States of America", "Vereinigte Staaten von Amerika", "États-Unis", "Estados Unidos"]
   end
-  
+
   it 'should return latitude' do
     country.latitude.should == '38 00 N'
   end
-  
+
   it 'should return longitude' do
     country.longitude.should == '97 00 W'
   end
@@ -39,50 +39,50 @@ describe ISO3166::Country do
   it 'should return subregion' do
     country.subregion.should == 'Northern America'
   end
-  
+
   describe 'e164' do
     it 'should return country_code' do
       country.country_code.should == '1'
     end
-    
+
     it 'should return national_destination_code_lengths' do
       country.national_destination_code_lengths.should == [3]
     end
-    
+
     it 'should return national_number_lengths' do
       country.national_number_lengths.should == [10]
-    end               
-                      
+    end
+
     it 'should return international_prefix' do
       country.international_prefix.should == '011'
-    end               
-                      
+    end
+
     it 'should return national_prefix' do
       country.national_prefix.should == '1'
     end
   end
-  
+
   describe 'subdivisions' do
     it 'should return an empty hash for a country with no ISO3166-2' do
       ISO3166::Country.search('VA').subdivisions.should have(0).subdivisions
     end
-    
+
     it 'should return a hash with all sub divisions' do
       country.subdivisions.should have(57).states
     end
   end
-  
+
   describe 'search' do
     it 'should return new country object when a valid alpha2 is passed' do
       ISO3166::Country.search('US').should be_a(ISO3166::Country)
     end
   end
-  
+
   describe 'currency' do
     it 'should return an instance of Currency' do
       country.currency.should be_a(ISO4217::Currency)
     end
-    
+
     it 'should allow access to symbol' do
       country.currency[:symbol].should == '$'
     end
