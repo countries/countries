472756cd1eac0a5f0fe05e750b04698ab372a4f8
Marcin Michalowski and Marcin Nowicki <daan+pr0d1r2+experteer-devs@ragnarson.com>
2011-02-09 09:51:58 +0100

    Cleanup spec by using let.
    Also speeds up and give "lazy" usage of "country".

--- spec/country_spec.rb
+++ spec/country_spec.rb
@@ -1,65 +1,64 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe ISO3166::Country do
-  before(:all) do
-    @country = ISO3166::Country.search('US')
-  end
+
+  let(:country) { ISO3166::Country.search('US') }
   
   it 'should return 3166 number' do
-    @country.number.should == '840'
+    country.number.should == '840'
   end
   
   it 'should return 3166 alpha2 code' do
-    @country.alpha2.should == 'US'
+    country.alpha2.should == 'US'
   end
   
   it 'should return 3166 alpha3 code' do
-    @country.alpha3.should == 'USA'
+    country.alpha3.should == 'USA'
   end
   
   it 'should return 3166 name' do
-    @country.name.should == 'United States'
+    country.name.should == 'United States'
   end
   
   it 'should return alternate names' do
-    @country.names.should == ["United States of America", "Vereinigte Staaten von Amerika", "États-Unis", "Estados Unidos"]
+    country.names.should == ["United States of America", "Vereinigte Staaten von Amerika", "États-Unis", "Estados Unidos"]
   end
   
   it 'should return latitude' do
-    @country.latitude.should == '38 00 N'
+    country.latitude.should == '38 00 N'
   end
   
   it 'should return longitude' do
-    @country.longitude.should == '97 00 W'
+    country.longitude.should == '97 00 W'
   end
 
   it 'should return region' do
-    @country.region.should == 'Americas'
+    country.region.should == 'Americas'
   end
 
   it 'should return subregion' do
-    @country.subregion.should == 'Northern America'
+    country.subregion.should == 'Northern America'
   end
   
   describe 'e164' do
     it 'should return country_code' do
-      @country.country_code.should == '1'
+      country.country_code.should == '1'
     end
     
     it 'should return national_destination_code_lengths' do
-      @country.national_destination_code_lengths.should == [3]
+      country.national_destination_code_lengths.should == [3]
     end
     
     it 'should return national_number_lengths' do
-      @country.national_number_lengths.should == [10]
+      country.national_number_lengths.should == [10]
     end               
                       
     it 'should return international_prefix' do
-      @country.international_prefix.should == '011'
+      country.international_prefix.should == '011'
     end               
                       
     it 'should return national_prefix' do
-      @country.national_prefix.should == '1'
+      country.national_prefix.should == '1'
     end
   end
   
@@ -69,7 +68,7 @@ describe ISO3166::Country do
     end
     
     it 'should return a hash with all sub divisions' do
-      @country.subdivisions.should have(57).states
+      country.subdivisions.should have(57).states
     end
   end
   
@@ -81,11 +80,11 @@ describe ISO3166::Country do
   
   describe 'currency' do
     it 'should return an instance of Currency' do
-      @country.currency.should be_a(ISO4217::Currency)
+      country.currency.should be_a(ISO4217::Currency)
     end
     
     it 'should allow access to symbol' do
-      @country.currency[:symbol].should == '$'
+      country.currency[:symbol].should == '$'
     end
   end
 
