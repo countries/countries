8d1e94c6e1c17063e3a8d959dec7f84524794b54
Sean Doig <sean.doig@gmail.com>
2013-08-15 15:27:11 +0100

    Refactorings

--- lib/countries/mongoid.rb
+++ lib/countries/mongoid.rb
@@ -1,6 +1,10 @@
-module ISO3166; end
+module ISO3166
+  ;
+end
 
 class ISO3166::Country
+  class BadMongoidTypeError < StandardError;
+  end
 
   def mongoize
     ISO3166::Country.mongoize(self)
@@ -8,16 +12,34 @@ class ISO3166::Country
 
   class << self
 
-    def mongoize(country_object)
-      country_object.alpha2
+    def mongoize(country)
+      if country.is_a?(self) && !country.data.nil?
+        country.alpha2
+      elsif self.send(:valid_alpha2?, country)
+        new(country).alpha2
+      else
+        raise BadMongoidTypeError.new('Given value is neither a valid country object nor a valid alpha2 code')
+      end
     end
 
     def demongoize(alpha2)
-      ISO3166::Country.new(alpha2)
+      new(alpha2)
     end
 
-    def evolve(country_object)
-      mongoize(country_object)
+    def evolve(country)
+      mongoize(country)
+    end
+
+    private
+
+    def valid_alpha2?(country)
+      return false unless country.is_a?(String)
+
+      if ISO3166::Country.new(country).nil?
+        raise BadMongoidTypeError.new('Given string is not a valid alpha2 code.')
+      else
+        true
+      end
     end
   end
 end
