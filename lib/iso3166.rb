711408daed4716103a58d068a36babc0d04031f7
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2014-09-15 13:22:32 +0200

    change ActionView constant silently

--- lib/iso3166.rb
+++ lib/iso3166.rb
@@ -4,7 +4,7 @@ require 'iso4217'
 require 'countries/country'
 
 if defined?(ActionView::Helpers::FormOptionsHelper)
-  Kerner.silence_warnings do
+  Kernel.silence_warnings do
     ActionView::Helpers::FormOptionsHelper::COUNTRIES = ISO3166::Country::Names.map{ |(name,alpha2)| [name.html_safe,alpha2] }
   end
 end
