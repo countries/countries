5988bc6b86b660b58f695178ce63e0949b05cb72
Alexey Zapparov <ixti@member.fsf.org>
2015-02-11 20:46:07 +0100

    Expose Country only when it's safe

--- lib/countries.rb
+++ lib/countries.rb
@@ -3,8 +3,10 @@ require 'countries/version'
 require 'iso3166'
 require 'countries/mongoid' if defined?(Mongoid)
 
-class Country < ISO3166::Country
-  def to_s
-    name
+unless defined? Country
+  class Country < ISO3166::Country
+    def to_s
+      name
+    end
   end
 end
