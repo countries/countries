3ad01c283b64a9643de869ce1e9ffdfda57e2bd4
Russell Osborne <russell@burningpony.com>
2015-08-20 15:13:35 -0400

    Remove deprecation from Country call

--- lib/global.rb
+++ lib/global.rb
@@ -6,7 +6,6 @@ require 'countries/mongoid' if defined?(Mongoid)
 
 class Country < ISO3166::Country
   def to_s
-    warn "[DEPRECATION] `Country` is deprecated.  Please use `ISO3166::Country` instead."
     name
   end
 end
