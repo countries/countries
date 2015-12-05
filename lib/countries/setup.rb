2820ab23c97bd6a4835ff6bebb64cab3457497ce
Russell Osborne <russell@burningpony.com>
2015-12-05 12:37:37 -0500

    Add some deprecation warnings for removed setup class

---
+++ lib/countries/setup.rb
@@ -0,0 +1,18 @@
+module ISO3166
+  ##
+  # <b>DEPRECATED:</b> Please use <tt>Data</tt> instead.
+  # TODO: Remove at version 2.1
+  # Handles building the in memory store of countries data
+  class Setup
+    # <b>DEPRECATED:</b> Please use <tt>Data.codes</tt> instead.
+    def codes
+      warn "[DEPRECATION] `useless` is deprecated.  Please use `useful` instead."
+      Data.codes
+    end
+
+    def data
+      warn "[DEPRECATION] `Setup.new.data` is deprecated without replacement
+      data is now loaded per locale Data.new(:en).call"
+    end
+  end
+end
