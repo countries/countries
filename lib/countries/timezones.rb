5e8b61be16f0199aad90bb7abed414b38294ecab
Russell Osborne <russell@burningpony.com>
2016-01-21 17:35:24 -0500

    Timezone extensions by default.

--- lib/countries/timezones.rb
+++ lib/countries/timezones.rb
@@ -1,10 +1,3 @@
-begin # require tzinfo
-  require 'tzinfo'
-rescue LoadError
-  puts 'WARNING: Failed to require tzinfo timezones will not be avaliable Please
-    add gem "tzinfo" to your gemfile'
-end
-
 # Extend Country class with support for timezones
 module ISO3166
   # Extend Country class with support for timezones
