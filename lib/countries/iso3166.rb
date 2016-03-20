295d078511e80b10119af152ca2cccd6844a0d8b
Russell Osborne <russell@theironyard.com>
2016-03-20 12:00:26 -0400

    Use struct over open_struct

--- lib/countries/iso3166.rb
+++ lib/countries/iso3166.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'i18n_data'
 require 'money'
 
+require 'countries/kwarg_struct'
 require 'countries/configuration'
 require 'countries/data'
 require 'countries/structure'
--- lib/countries/subdivision.rb
+++ lib/countries/subdivision.rb
@@ -1,5 +1,13 @@
 module ISO3166
-  class Subdivision < OpenStruct
-
-  end
+  Subdivision = KwargStruct.new(
+    :name,
+    :names,
+    :latitude,
+    :longitude,
+    :min_latitude,
+    :min_longitude,
+    :max_latitude,
+    :max_longitude,
+    :comments
+  )
 end
