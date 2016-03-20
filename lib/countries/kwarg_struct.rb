06b118ab7b33cf3009f3b899fd95448deb51d34a
Russell Osborne <russell@theironyard.com>
2016-03-20 16:41:52 -0400

    Fix ruby warning

--- lib/countries/kwarg_struct.rb
+++ lib/countries/kwarg_struct.rb
@@ -3,7 +3,7 @@ module ISO3166
     # Override the initialize to handle hashes of named parameters
     def initialize(*args)
       opts = args.last.is_a?(Hash) ? args.pop : Hash.new
-      super *args
+      super(*args)
       opts.each_pair do |k, v|
         self.send "#{k}=", v
       end
