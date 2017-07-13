3733179333c924babd2f8a166d3a777f0b81fd2f
Kaspars <kasparsj@gmail.com>
2017-07-13 23:42:50 +0300

    allow to override cache dir

--- lib/countries/data.rb
+++ lib/countries/data.rb
@@ -2,6 +2,7 @@ module ISO3166
   ##
   # Handles building the in memory store of countries data
   class Data
+    @@cache_dir = [File.dirname(__FILE__), 'cache']
     @@cache = {}
     @@registered_data = {}
 
@@ -14,6 +15,14 @@ module ISO3166
     end
 
     class << self
+      def cache_dir
+        @@cache_dir
+      end
+
+      def cache_dir=(value)
+        @@cache_dir = value
+      end
+
       def register(data)
         alpha2 = data[:alpha2].upcase
         @@registered_data[alpha2] = \
@@ -52,7 +61,7 @@ module ISO3166
 
       def load_data!
         return @@cache unless load_required?
-        @@cache = load_cache %w(cache countries.json)
+        @@cache = load_cache %w(countries.json)
         @@_country_codes = @@cache.keys
         @@cache = @@cache.merge(@@registered_data)
         @@cache
@@ -106,7 +115,7 @@ module ISO3166
       end
 
       def load_translations(locale)
-        locale_names = load_cache(['cache', 'locales', "#{locale}.json"])
+        locale_names = load_cache(['locales', "#{locale}.json"])
         internal_codes.each do |alpha2|
           @@cache[alpha2]['translations'] ||= Translations.new
           @@cache[alpha2]['translations'][locale] = locale_names[alpha2].freeze
@@ -129,7 +138,7 @@ module ISO3166
       end
 
       def datafile_path(file_array)
-        File.join([File.dirname(__FILE__)] + file_array)
+        File.join([@@cache_dir] + file_array)
       end
     end
   end
